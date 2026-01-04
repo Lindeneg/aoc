d3ddaa2aa3a43293dac6d3efa48f3dabc940baf8
lindeneg <christian@lindeneg.org>
2026-01-04 01:03:01 +0100

    update cl ts lib

--- cl/index.ts
+++ cl/index.ts
@@ -1,10 +1,14 @@
-export type {Nullable, AnyFn} from "./ts/types";
+export type {Nullable, AnyFn, Point2, Point3} from "./ts/types";
 export {default as Day, Result} from "./ts/day";
 export {default as Printable, type Stringable} from "./ts/printable";
 export {default as Vec2} from "./ts/vec2";
 export {default as Vec3} from "./ts/vec3";
+export {default as Rect} from "./ts/rect";
+export {default as Polygon2} from "./ts/polygon2";
+export {default as Vec2Compressor} from "./ts/vec2-compressor";
 export {
     DenseGrid2,
+    SparseGrid2,
     UP,
     RIGHT,
     DOWN,
@@ -25,3 +29,4 @@ export {
     type UfMode,
     type Uf,
 } from "./ts/uf";
+export {orient2, pointOnSegment2, segmentsIntersect2} from "./ts/geometry";
