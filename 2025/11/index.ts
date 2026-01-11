dde5e75fff90276bc29b8e299c4731c5636bdeac
lindeneg <christian@lindeneg.org>
2026-01-11 04:28:08 +0100

    day 11 initial

---
+++ 2025/11/index.ts
@@ -0,0 +1,17 @@
+import {Day} from "../../cl";
+
+const day11 = new Day(
+    (part, data: string[]) => {
+        let answer = 0;
+        return answer;
+    },
+    [null, null],
+    [null, null]
+).setPostTransform((transformed) => {
+    return transformed;
+});
+
+(async () => {
+    await day11.examples(1);
+    //await day11.solve();
+})();
