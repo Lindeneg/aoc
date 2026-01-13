46999aa81ae30438ec8dc732c1c3d16f47b2a917
lindeneg <christian@lindeneg.org>
2026-01-13 15:45:05 +0100

    start on day 11

--- 2025/11/index.ts
+++ 2025/11/index.ts
@@ -1,17 +1,77 @@
-import {Day} from "../../cl";
+import {Day, Vertex, VertexGraph, unwrap, GRAPH_MODE} from "../../cl";
+
+// TODO dynamiaclly build the graph, part 2 gets really slow.
+
+class DeviceVertex extends Vertex<string> {
+    constructor(config: string) {
+        super(config);
+    }
+}
+
+type Graph = VertexGraph<typeof DeviceVertex, (v: string) => string>;
 
 const day11 = new Day(
-    (part, data: string[]) => {
-        let answer = 0;
-        return answer;
+    (
+        _,
+        graph: Graph,
+        start: string,
+        end: string,
+        mustContain: string[] = []
+    ) => {
+        return distinctPathCount(graph, start, end, mustContain);
     },
-    [null, null],
-    [null, null]
+    [674, null],
+    [5, {path: "./example2.in", want: 2}]
 ).setPostTransform((transformed) => {
-    return transformed;
+    const graph: Graph = new VertexGraph(
+        DeviceVertex,
+        (v) => v,
+        GRAPH_MODE.DIRECTED
+    );
+    for (const line of transformed) {
+        let [vertexData, ...edges] = line.split(" ");
+        vertexData = vertexData.slice(0, vertexData.length - 1);
+        graph.addVertex(vertexData);
+        for (const edgeData of edges) {
+            graph.addVertex(edgeData);
+            unwrap(graph.addEdgeFromData(vertexData, edgeData));
+        }
+    }
+
+    return graph;
 });
 
+function distinctPathCount(
+    graph: Graph,
+    start: string,
+    end: string,
+    mustContain: string[],
+    path: string[] = []
+) {
+    let answer = 0;
+    const startVertex = graph.getVertex(start)!;
+    for (const edge of startVertex.edges) {
+        path.push(edge.next.data);
+        if (graph.hash(end) === graph.hash(edge.next.data)) {
+            const hasRequired = mustContain.reduce((result, required) => {
+                return result && path.includes(required);
+            }, true);
+            return Number(hasRequired);
+        }
+        answer += distinctPathCount(
+            graph,
+            edge.next.data,
+            end,
+            mustContain,
+            path
+        );
+    }
+    return answer;
+}
+
 (async () => {
-    await day11.examples(1);
-    //await day11.solve();
+    console.log(await day11.examples(1, "you", "out"));
+    console.log(await day11.examples(2, "svr", "out", ["dac", "fft"]));
+    //    console.log(await day11.solve(1, "you", "out"));
+    // console.log(await day11.solve(2, "svr", "out", ["dac", "fft"]));
 })();
